1c3eecf589cebc4e925ca370dfa5ce731e9af6f6
Julija Lee <lee.julija@gmail.com>
2017-09-30 00:55:53 +0000

    adding 2, need to fix carry over to next line

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,11 +1,20 @@
 $(document).ready(function () {
+  let amensObj = {}
+  let amensStr = '';
   $("input:checkbox").change(function() {
     if($(this).is(':checked')) {
+      id = $(this).attr('data-id');
       name = $(this).attr('data-name');
-      $('.amenities h4').append(name);
+      amensObj[id] = name;
     }
     else {
-	alert("Nope");
+      id = $(this).attr('data-id');
+      delete amensObj[id];
     }
+    amensStr = '';
+    for(let key in amensObj) {
+      amensStr = amensStr.concat(amensObj[key], ', ');
+    }
+    $('.amenities h4').text(amensStr.substring(0, amensStr.length-2));
   });
 });
