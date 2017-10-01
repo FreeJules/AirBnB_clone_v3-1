4eeaff32e9325eb02f79216fbc322bc7cd70cd85
Julija Lee <lee.julija@gmail.com>
2017-10-01 02:44:25 +0000

    adding js for 3

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  /* adding checked amenities to .amenities */
+  let amensObj = {};
+  let amensStr = '';
+  $('input:checkbox').change(function () {
+    if ($(this).is(':checked')) {
+      let id = $(this).attr('data-id');
+      let name = $(this).attr('data-name');
+      amensObj[id] = name;
+    } else {
+      let id = $(this).attr('data-id');
+      delete amensObj[id];
+    }
+    amensStr = '';
+    for (let key in amensObj) {
+      amensStr = amensStr.concat(amensObj[key], ', ');
+    }
+    $('.amenities h4').text(amensStr.substring(0, amensStr.length - 2));
+  });
+  /* checking if status is ok for API */
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, status) {
+    if (status === 'success') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
+  });
+});
