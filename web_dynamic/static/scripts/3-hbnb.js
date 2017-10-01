236f73a47f29cacfaeaa5d005235b81b3856d532
Julija Lee <lee.julija@gmail.com>
2017-10-01 11:58:51 +0000

    adding task 4

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -29,48 +29,51 @@ $(document).ready(function () {
   $.ajax({
     type: 'POST',
     url: 'http://0.0.0.0:5001/api/v1/places_search/',
-    contentType: "application/json",
+    contentType: 'application/json',
     dataType: 'json',
     data: JSON.stringify({}),
-    success: function(data) {
-      console.log(data);
-      $.each(data, function(index, place) {
+    success: function (data) {
+      $.each(data, function (index, place) {
         $('<article id="' + place.id + '"></article>').appendTo('.places');
-	let plIdStr = '.places #' + place.id
+        let plIdStr = '.places #' + place.id;
         $('<div class="title"></div>').appendTo(plIdStr);
-	$('<div class="information"></div>').appendTo(plIdStr);
-	$('<div class="user"></div>').appendTo(plIdStr);
-	$('<div class="description"></div>').appendTo(plIdStr);
-	/*
-	adding name and price
-	class='title' and class='price_by_night'
-	*/
-	$(plIdStr + ' .title').append('<h2>' + place.name + '</h2>');
-	$(plIdStr + ' .title').append('<div class="price_by_night">$' + place.price_by_night + '</div>');
-        /*
-        getting second div with class+'information'
-        classes: 'max_guest', 'number_bathrooms', 'nhumber_rooms'
-        */
+        $('<div class="information"></div>').appendTo(plIdStr);
+        $('<div class="user"></div>').appendTo(plIdStr);
+        $('<div class="description"></div>').appendTo(plIdStr);
+        /* adding name and price class='title' and class='price_by_night' */
+        $(plIdStr + ' .title').append('<h2>' + place.name + '</h2>');
+        $(plIdStr + ' .title').append('<div class="price_by_night">$' + place.price_by_night + '</div>');
+        /* adding class information, classes: 'max_guest', 'number_bathrooms', 'nhumber_rooms' */
         $(plIdStr + ' .information').append('<div class="max_guest"></div>');
-	$(plIdStr + ' .information .max_guest').
-	      append('<i class="fa fa-users fa-3x" aria-hidden="true"></i>');
-	$(plIdStr + ' .information .max_guest').append('<br />');
+        $(plIdStr + ' .information .max_guest').append('<i class="fa fa-users fa-3x" aria-hidden="true"></i>');
+        $(plIdStr + ' .information .max_guest').append('<br />');
         $(plIdStr + ' .information .max_guest').append(place.max_guest + ' Guests');
 
-	$(plIdStr + ' .information').append('<div class="number_rooms"></div>');
-        $(plIdStr + ' .information .number_rooms').
-              append('<i class="fa fa-bed fa-3x" aria-hidden="true"></i>');
+        $(plIdStr + ' .information').append('<div class="number_rooms"></div>');
+        $(plIdStr + ' .information .number_rooms').append('<i class="fa fa-bed fa-3x" aria-hidden="true"></i>');
         $(plIdStr + ' .information .number_rooms').append('<br />');
         $(plIdStr + ' .information .number_rooms').append(place.number_rooms + ' Rooms');
 
         $(plIdStr + ' .information').append('<div class="number_bathrooms"></div>');
-        $(plIdStr + ' .information .number_bathrooms').
-              append('<i class="fa fa-bath fa-3x" aria-hidden="true"></i>');
+        $(plIdStr + ' .information .number_bathrooms').append('<i class="fa fa-bath fa-3x" aria-hidden="true"></i>');
         $(plIdStr + ' .information .number_bathrooms').append('<br />');
         $(plIdStr + ' .information .number_bathrooms').append(place.number_bathrooms + ' Bathrooms');
+        /* adding class user */
+        $(plIdStr + ' .user').append('<strong>Owner: </strong>');
+        let userUrl = 'http://0.0.0.0:5001/api/v1/users/' + place.user_id;
+        $.get(userUrl, function (userData) {
+          $(plIdStr + ' .user').append(userData.first_name + ' ' + userData.last_name);
+        });
+        /* adding class description */
+        let paragraphList = place.description.split('<BR />');
+        console.log(paragraphList);
+        for (let paragraph in paragraphList) {
+          $(plIdStr + ' .description').append(paragraphList[paragraph]);
+          $(plIdStr + ' .description').append('<br />');
+        }
       });
     },
-    error: function(data) {
+    error: function (data) {
       console.log(data);
     }
   });
